d20b61c7b18afcb2e855ffeea477d3e8058979e8
psullivan6 <psullivan6@gmail.com>
2013-06-05 12:01:16 -0700

    JS idea
    first stab at the js code for "global" event tracking via data
    attributes

---
+++ wiretap.js
@@ -0,0 +1,23 @@
+// Code ideas:
+
+
+// Since 'category' is required for event tracking, the flag can be for
+// this data attribute and the rest can be achieved through checking
+// 'this'. Wrap in a window.load so that it is always called after the main
+GA code. $('a').attr('data-ga-category').click(function(){
+	
+	$this       = $(this),
+	ga_category = $this.attr('data-ga-category'),
+	ga_action   = $this.attr('data-ga-action'),
+	ga_label    = $this.attr('data-ga-label'),
+	ga_value    = $this.attr('data-ga-value');
+	
+	_gaq.push(['_trackEvent', ga_category, ga_action, ga_label, ga_value])
+	
+});
+
+/*
+ * Use data-ga-___ like this:
+ * <a href="#" data-ga-category="foo" data-ga-action="baz" data-ga-label="bar" data-ga-value="xxx">My link</a>
+ *
+ */
